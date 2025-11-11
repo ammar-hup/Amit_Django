c52d5bf84a0f57ae1fb5360edf42d071dedba088
ammar-hup <ammar.mahmod117@gmail.com>
2025-11-11 18:42:39 +0200

    sesssion 4 ended: create new post functionality added sucsessfully

--- blog/forms.py
+++ blog/forms.py
@@ -11,7 +11,39 @@ class PostForm(forms.ModelForm):
             'published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
             'comments': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Add comments...'}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            # 'author': forms.Select(attrs={'class': 'form-select'}),
         }
         labels = {
             'published': 'Draft',
         }
+    
+    # def clean_title(self):
+    #     title = self.cleaned_data.get('title')
+    #     if len(title) < 5:
+    #         raise forms.ValidationError("Title must be at least 5 characters.")
+    #     return title
+
+    # def clean_content(self):
+    #     content = self.cleaned_data.get('content')
+    #     if len(content.split()) < 5:
+    #         raise forms.ValidationError("Content must be at least 20 words.")
+    #     return content
+
+# class SignUpForm(UserCreationForm):
+#     email = forms.EmailField(required=True)
+#     first_name = forms.CharField(max_length=30)
+#     last_name = forms.CharField(max_length=30)
+
+#     class Meta:
+#         model = Author
+#         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+#     def save(self, commit=True):
+#         user = super().save(commit=False)
+#         user.email = self.cleaned_data['email']
+#         if commit:
+#             user.save()
+#         return user
+
+
+
